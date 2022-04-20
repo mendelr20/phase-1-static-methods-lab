8ce6899132df1f78cae6c0a67dec3157a01b9d26
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-04-20 14:55:27 -0400

    commit tree 86e4acea2fbf0d70a7503288c62683392a0fa01c

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, "");
+  }
+
+  static titleize(string){
+    const splitString = string.split(' ')
+    const wordList = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let capitalArray = []
+    for (let i = 1; i < splitString.length; i++){
+      if((wordList.find(element => element === splitString[i])) !== splitString[i]){
+        capitalArray.push(this.capitalize(splitString[i]));
+      } else {
+        capitalArray.push(splitString[i])
+      }
+    }
+    capitalArray.unshift(this.capitalize(splitString[0]))
+    return capitalArray.join(' ')
+  }
 }
